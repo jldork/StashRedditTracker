f4f51701fbdcf8ca299e4123afd8f58677cae74c
James Leung <jleung@stashinvest.com>
2019-10-07 09:47:01 -0400

    HDP Model Working
    - 23 meaningful topics out of 150
    - However, 4 main dominant topics
    - Generally not comprehendable

--- tasks/train_and_score_model.py
+++ tasks/train_and_score_model.py
@@ -3,7 +3,7 @@ import pickle
 import pandas as pd
 import matplotlib.pyplot as plt
 from helpers.timer import Timer
-from gensim.models import CoherenceModel
+from gensim.models import CoherenceModel, HdpModel
 from gensim.models.wrappers import LdaMallet
 from gensim.corpora.dictionary import Dictionary
 
@@ -79,19 +79,15 @@ def get_topic_model_scores(df,model, seen=True):
     
     So we have to translate these into columns for our dataframe
     """
-    scores = model.load_document_topics() if seen else model[df['bow']]
+    scores = df['bow'].apply(lambda x: model[x])
+    topic_columns = [f"Topic: {i}" for i in range(model.get_topics().shape[0])]
+    df[topic_columns] = pd.DataFrame(0, index=df.index, columns=topic_columns)
     
-    i = 0;
-    # The index is in order, you can check with
-    # set(range(len(df.index))) - set(df.index)
-    for score in scores:
-        for per_topic_score in score:
-            topic_num = per_topic_score[0] + 1
-            topic_prob = per_topic_score[1]
-            df.loc[i,f'Topic: {topic_num}'] = topic_prob
-        i+=1
-    
-    topic_columns = [col for col in df.columns if col.startswith("Topic")]
+    for i in range(len(scores)):
+        topic_evaluation = scores[i]
+        for topic in topic_evaluation:
+            df.loc[i,f'Topic: {topic[0]}'] = topic[1]
+
     df['Topic'] = df[topic_columns].idxmax(axis=1).str.lstrip('Topic: ').astype(int)
     
     return df
@@ -101,41 +97,14 @@ def run():
     # Get the Preprocessed Dataset
     prepared_df = pd.read_pickle('./data/tmp/preprocessed.pkl')
     df, corpus, dictionary = get_corpus_and_dict(prepared_df, 'tokens')
-        
-    if os.path.isfile('./models/MALLET/mallet_model.pkl'):
-        # Let's not do any model retraining without building in topic stability constraints
-        #     e.g. number of docs or tokens now in different topics
-        
-        seen = False # Data we provide is new and unseen for the model
-        with open('models/MALLET/mallet_model.pkl','rb') as modelfile:
-            topic_model = pickle.load(modelfile)
-        
-    else:
-        
-        
-        seen = True # any data we provide is used to train the model
-        with Timer('Train the LDA Model'):
-            test_range = (5,50)
-            list_of_models, scores = topic_count_selection(
-                dictionary, corpus, list(df['tokens']), test_range
-            )
 
-            plot_coherence(test_range, scores).savefig('./models/MALLET/ModelCoherence.png')
-
-            # Let's save the model with highest coherence
-            num_topics = rtest_range[0]+scores.index(max(scores))
-            topic_model = list_of_models[num_topics]
-            
-            print(f"* Chosen Model with {num_topics} topics")
-            with open('./models/MALLET/mallet_model.pkl','wb') as modelfile:
-                pickle.dump(topic_model, modelfile)
-            with open('./models/MALLET/mallet_corpus.pkl','wb') as corpusfile:
-                pickle.dump(corpus, corpusfile)
-            with open('./models/MALLET/mallet_dict.pkl','wb') as dictfile:
-                pickle.dump(dictionary, dictfile)
+    with Timer('Train the LDA Model') as t, open('./models/HDP/hdp_model.pkl','wb') as modelfile:
+        hdp = HdpModel(corpus, dictionary)
+        df = get_topic_model_scores(df, hdp) 
         
-    df = get_topic_model_scores(df, topic_model, seen=seen)
-    df.to_pickle('./data/tmp/scored.pkl')
+        # Save the outputs
+        df.to_pickle('./data/tmp/scored.pkl')
+        pickle.dump(hdp, modelfile)
     
     print("\nSample")
     print(df.head(),"\n")
